39fe1551a82d49c7d9a80dcf614288df5122a704
Rose <rosenjdk@gmail.com>
2026-09-13 21:00:02 -0600

    Connect API

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const searchButton = document.getElementById("search-button");
 searchButton.addEventListener("click", function () {
     console.log(search.value);
 });
+const API_URL = "https://api.jikan.moe/v4/anime?q=";
