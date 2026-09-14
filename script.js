34de7b9f8e0804a8077986c6ee31a1aae7f55cde
Rose <rosenjdk@gmail.com>
2026-09-13 19:51:01 -0600

    fixing button

---
+++ script.js
@@ -0,0 +1,5 @@
+const search = document.getElementById("search");
+const searchButton = document.getElementById("search-button");
+searchButton.addEventListener("click", function () {
+    console.log(search.value);
+});
